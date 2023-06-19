f8f3e2de4f169ac1b31cd24f487c4b95b52cae99
Sumit <sumitgupta19900@gmail.com>
2023-06-19 23:59:06 +0530

    19

---
+++ MERN/notes.js
@@ -0,0 +1,18 @@
+/*
+1. useEffect:: whenever we want to interact with outside world we use 
+
+
+#NEXT JS
+> to create a Next Js app::
+npx create-next-app
+
+#>> 
+1. useRouter from"next/router : If you want to access the router object inside any function component in your app, you can use the useRouter hook 
+
+2.npm install semantic-ui-react semantic-ui-css
+  import 'semantic-ui-css/semantic.min.css'      :: Semantic UI React is the official React integration for Semantic UI 
+
+3.  
+
+
+*/
