017879219add0785cf0716b960c2c18748838ee6
Sumit <sumitgupta19900@gmail.com>
2023-06-24 12:35:49 +0530

    24

--- MERN/notes.js
+++ MERN/notes.js
@@ -2,7 +2,8 @@
 1. useEffect:: whenever we want to interact with outside world we use this
 2. getInitalProps
 
-#NEXT JS
+                             #NEXT JS
+
 > to create a Next Js app::
 npx create-next-app
 
@@ -14,5 +15,25 @@ npx create-next-app
 
 3. axios
 
+-----------------------------------------------------------------------------------------------------------------------------
+
+
+                             #NODE JS 
+
+:Node,js is an open-source, cross-platform JavaScript runtime environment and library for running web applications outside the client’s browse.Node.js allows you to run JavaScript on the server.
+:It is used for creating server-side web applications
+:Node.js is perfect for data-intensive applications as it uses an asynchronous, event-driven model.
+:Node.js runs single-threaded, non-blocking, asynchronous programming, which is very memory efficient.
+
+>>What Can Node.js Do?
+Node.js can generate dynamic page content
+Node.js can create, open, read, write, delete, and close files on the server
+Node.js can collect form data
+Node.js can add, delete, modify data in your database
+
+>>Node.js files contain tasks that will be executed on certain events. A typical event is someone trying to access a port on the server
+
+
+ 
 
 */
